9601b0b5472b11ce583dc4451cd76eb99712f5e1
elionaydiasgoncalves-lab <elionay.diasgoncalves@pditabira.com.br>
2026-04-12 21:44:41 -0300

    Add files via upload

---
+++ variavelll.py
@@ -0,0 +1,31 @@
+
+#questao2 a)
+x = 10
+y = x + 2
+print(y-5) #CORRETO
+
+#questao2 b)
+soma=( n1+nu)
+print soma  #CORRETO
+
+#questao2 c)
+a = 10
+a = a + 1
+print(a)
+
+#questao 2 d)
+saldo da conta = 1500 #esta errado, pois nao pode haver espaco teria que fazer saldo_da_conta = 1500
+0.1 = juros #  teria que atribuir valores a variavel juros = 0.1
+print(saldo da conta * juros)
+
+#questao 3
+num1 = 2
+num2 = 4
+num3 = 6
+media= (num1+ num2 + num3)/3
+print (media)
+
+#questao 5
+nome = " Elioenay jaiane"
+print ("meu nome e: ")
+print(nome)
